b87db710673b00ed81bac171cd76795725e2a95d
NaveenK11 <naveenkkarthik@yahoo.com>
2017-07-16 10:17:37 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -2,19 +2,19 @@
 # a function to call
 def prime_or_not():
      x = int(input("Enter a number: "))   # input a number
-     y = x-(x-2)                              # subtract with one because we know that the number is divisible by itself
+     y = x-(x-2)                          # subtract  because we know that the number is divisible by itself and one
 
-     while(y < x):                        # initiate while loop, since the numder is divisible by 1,skip it
-         if(x % y!= 0):                  # checks if x is not divisible by y and returns the following
+     while(y < x):                        # initiate while loop, since the numder is divisible by itself,skip it
+         if(x % y!= 0):                   # checks if x is not divisible by y and returns the following
              print(x , "is not divisible by",  y)
         
          else:
-             print(x, "is divisible by", y)
+             print(x, "is divisible by", y)  # else if divisible it breaks the loop
              print("Ooops!.....It is not a prime")
              break
          y = y+1
  
-         if(y==x):
-             print("Yay!...it is a prime")
-         
-prime_or_not()
+         if(y==x):                            #finally, if the looped y equals x then its proved that 
+             print("Yay!...it is a prime")    #it has not been divisible by any other number
+
+prime_or_not()                                #finally call the function
