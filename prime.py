e2042c5638bf82234912ba56762669058c948748
NaveenK11 <naveenkkarthik@yahoo.com>
2017-07-16 10:10:43 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,24 +1,20 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Jul 14 20:35:16 2017
-
-@author: naveen
-"""
+# a function to call
 def prime_or_not():
-     x=int(input("Enter a number: "))
-     y=x-1
+     x = int(input("Enter a number: "))   # input a number
+     y = x-(x-2)                              # subtract with one because we know that the number is divisible by itself
 
-     while(y>1):
-         if((x%y!=0)):
+     while(y < x):                        # initiate while loop, since the numder is divisible by 1,skip it
+         if(x % y!= 0):                  # checks if x is not divisible by y and returns the following
              print(x , "is not divisible by",  y)
         
          else:
              print(x, "is divisible by", y)
              print("Ooops!.....It is not a prime")
              break
-         y=y-1
+         y = y+1
  
-     if(y==1):
-         print("Yay!...it is a prime")
+         if(y==x):
+             print("Yay!...it is a prime")
          
 prime_or_not()
