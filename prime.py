7c10e0f25ad25df2fbfe7027e32431ab133d14e3
NaveenK11 <naveenkkarthik@yahoo.com>
2017-07-17 09:11:54 -0400

    Update prime.py

--- prime.py
+++ prime.py
@@ -18,6 +18,3 @@ def prime_or_not():
          if(y==x):                            #finally, if the looped y equals x then its proved that 
              print("Yay!...it is a prime")    #it has not been divisible by any other number
 
-if __name__ == "__main__":
-    import sys
-    prime_or_not(int(sys.argv[1]))
